3447c1bfda7dffc6523b59245b71bd612c6a3efe
Wawan Diputra <wawan.diputra@mitrais.com>
2017-03-13 13:18:53 +0800

    initial code

--- myService/handler.js
+++ myService/handler.js
@@ -1,15 +1,31 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Yeay !!! Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
 
-  callback(null, response);
+// const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const services = require('./services');
+let DynamoDbDataService = services.DynamoDbDataService;
+
+module.exports.list = (event, context, callback) => {
+  const TABLE_NAME = 'tempdb';
+  const NUMBER_OF_ITEMS = 50;
+  let dynamoDbDataService = new DynamoDbDataService(TABLE_NAME, NUMBER_OF_ITEMS);
+
+  dynamoDbDataService.getAll()
+    .then((results) => {
+      const response = {
+        headers: {
+          "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+          "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+        },
+        statusCode: 200,
+        body: JSON.stringify(results),
+      };
+      callback(null, response);
+    })
+    .catch((error) => {
+      callback(error);
+    });
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
---
+++ myService/services/dynamodb.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let AWS = require("aws-sdk");
+
+class DynamoDbDataService {
+    constructor(tableName, numberOfItems){
+        this.tableName = tableName;
+        this.numberOfItems = numberOfItems;
+
+        const awsDefaultRegion = process.env.AWS_DEFAULT_REGION;
+
+        let dynamoDbParams = {
+            region: awsDefaultRegion
+        };
+
+        AWS.config.update(dynamoDbParams);
+    }
+
+    /**
+     * Retrieve all items from table
+     */
+    getAll(){
+        return new Promise((resolve, reject)=>{
+            let docClient = new AWS.DynamoDB.DocumentClient();
+            const params = { 
+                TableName: this.tableName, 
+                Limit: this.numberOfItems 
+            };
+            
+            docClient.scan(params, (err, data) => {
+                if (err){
+                    reject(err);
+                }
+                else{
+                    resolve(data);
+                }
+            });
+        });
+    }
+}
+
+module.exports = DynamoDbDataService
